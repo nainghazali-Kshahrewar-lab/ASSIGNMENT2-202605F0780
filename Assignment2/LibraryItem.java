10fd30713be8d0c0d029cdc304d277a1655ad41a
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-08-04 11:28:52 +0000

    Create LibraryItem.java

---
+++ Assignment2/LibraryItem.java
@@ -0,0 +1,46 @@
+package librarymanagement;
+
+public abstract class LibraryItem {
+    // Private attributes - Encapsulation
+    private String itemId;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public LibraryItem(String itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;  
+    }
+
+
+    public String getItemId() { return itemId; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isAvailable() { return isAvailable; }
+
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+
+    public void displayInfo() {
+        System.out.println("Item ID: " + itemId);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Available: " + (isAvailable ? "Yes" : "No"));
+    }
+
+
+    public abstract double getLateFee(int daysLate);
+
+
+    public abstract String getItemType();
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s by %s", itemId, title, author);
+    }
+}
