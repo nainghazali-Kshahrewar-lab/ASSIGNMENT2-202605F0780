19fd0e81cdf94ee52eea4215a2ee5e34b4398be8
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-08-04 20:23:09 +0800

    Update DVD.java

--- Assignment2/DVD.java
+++ Assignment2/DVD.java
@@ -1,15 +1,12 @@
-package librarymanagement;
-
 public class DVD extends LibraryItem {
     private String director;
-    private int runtime;  
+    private int runtime;
 
     public DVD(String itemId, String title, String author, String director, int runtime) {
         super(itemId, title, author);
         this.director = director;
         this.runtime = runtime;
     }
-
 
     public String getDirector() { return director; }
     public int getRuntime() { return runtime; }
@@ -26,8 +23,6 @@ public class DVD extends LibraryItem {
         }
     }
 
-
-    @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Type: DVD");
@@ -35,18 +30,14 @@ public class DVD extends LibraryItem {
         System.out.println("Runtime: " + runtime + " minutes");
     }
 
-
-    @Override
     public double getLateFee(int daysLate) {
         return daysLate * 1.00;
     }
 
-    @Override
     public String getItemType() {
         return "DVD";
     }
 
-    @Override
     public String toString() {
         return super.toString() + " (DVD)";
     }
