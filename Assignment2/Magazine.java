2407407f3c0380d43bed7c130b50acb95106fec6
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-08-04 21:05:16 +0800

    Add setter for issueNumber and override toString method

--- Assignment2/Magazine.java
+++ Assignment2/Magazine.java
@@ -8,17 +8,31 @@ public class Magazine extends LibraryItem {
 
     public int getIssueNumber() { return issueNumber; }
 
+    public void setIssueNumber(int issueNumber) {
+        if (issueNumber > 0) {
+            this.issueNumber = issueNumber;
+        }
+    }
+
+    @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Type: Magazine");
         System.out.println("Issue Number: " + issueNumber);
     }
 
+    @Override
     public double getLateFee(int daysLate) {
         return daysLate * 0.30;
     }
 
+    @Override
     public String getItemType() {
         return "Magazine";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Magazine)";
     }
 }
