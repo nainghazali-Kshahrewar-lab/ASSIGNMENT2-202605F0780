3d7f2a962deb9a81572702008020c4983c58a3d2
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-08-04 21:05:33 +0800

    Add setter methods for director and runtime

--- Assignment2/DVD.java
+++ Assignment2/DVD.java
@@ -11,6 +11,19 @@ public class DVD extends LibraryItem {
     public String getDirector() { return director; }
     public int getRuntime() { return runtime; }
 
+    public void setDirector(String director) {
+        if (director != null && !director.trim().isEmpty()) {
+            this.director = director;
+        }
+    }
+
+    public void setRuntime(int runtime) {
+        if (runtime > 0) {
+            this.runtime = runtime;
+        }
+    }
+
+    @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Type: DVD");
@@ -18,11 +31,18 @@ public class DVD extends LibraryItem {
         System.out.println("Runtime: " + runtime + " minutes");
     }
 
+    @Override
     public double getLateFee(int daysLate) {
         return daysLate * 1.00;
     }
 
+    @Override
     public String getItemType() {
         return "DVD";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (DVD)";
     }
 }
