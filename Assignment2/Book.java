4155b467147314d4427edbf2fcd73da8d534158c
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-08-04 20:23:25 +0800

    Update Book.java

--- Assignment2/Book.java
+++ Assignment2/Book.java
@@ -1,16 +1,12 @@
-package librarymanagement;
-
 public class Book extends LibraryItem {
     private String isbn;
     private int pages;
-
 
     public Book(String itemId, String title, String author, String isbn, int pages) {
         super(itemId, title, author);
         this.isbn = isbn;
         this.pages = pages;
     }
-
 
     public String getIsbn() { return isbn; }
     public int getPages() { return pages; }
@@ -27,27 +23,21 @@ public class Book extends LibraryItem {
         }
     }
 
-
-    @Override
     public void displayInfo() {
-        super.displayInfo();  
+        super.displayInfo();
         System.out.println("Type: Book");
         System.out.println("ISBN: " + isbn);
         System.out.println("Pages: " + pages);
     }
 
-
-    @Override
     public double getLateFee(int daysLate) {
         return daysLate * 0.50;
     }
 
-    @Override
     public String getItemType() {
         return "Book";
     }
 
-    @Override
     public String toString() {
         return super.toString() + " (Book)";
     }
